4f74b150e5f44547ca8b01d7ca262f5739942572
JuanUrrego02 <juandavid54300@gmail.com>
2026-04-09 18:46:38 -0500

    Register changes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,14 @@
 import { Router } from 'express';
-import { login } from '../controllers/auth.controller.js';
+import { login, register} from '../controllers/auth.controller.js';
 const router = Router();
 /**
 * POST /api/auth/login
 */
 router.post('/login', login);
+
+/**
+* POST /api/auth/register
+*/
+router.post('/register', register);
 
 export default router;
